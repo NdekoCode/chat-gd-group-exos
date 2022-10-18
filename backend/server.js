fdfdb58ea50cde855f7980b033226cd5f272cecf
GloiMir <gloirmirindi@gmail.com>
2022-10-18 14:21:17 +0200

    refact:backend first commit

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,36 @@
+// const http = require('http')
+// const fs = require('fs')
+// const events = require('events')
+// const MonEvent = new events()
 
+// // MonEvent.on('connecter',()=>{console.log("Un user vient de se connecter")})
+
+// const serveur = http.createServer()
+// // MonEvent.emit('connecter')
+
+// const tab = [
+//     { id: '', content: '', created_at: '' },
+//     { id: '', content: '', created_at: '' },
+//     { id: '', content: '', created_at: '' },
+//     { id: '', content: '', created_at: '' },
+// ]
+
+// serveur.on('request', (req, res) => {
+//     // res.writeHead(200,{'Content-Type':'application/json'})
+
+//     fs.readFile('./index.json', (err, data) => {
+//         if (err) throw err;
+//         // res.writeHead(200,{
+//         //     'Content-Type':'Text/html'
+//         // })
+//         res.setHeader("Access-Control-Allow-Origin", "*")
+//         res.setHeader("Access-Control-Allow-Headers", "*")
+//         res.setHeader("Access-Control-Allow-methods", "POST,GET,PUT,OPTIONS")
+//         res.write(data)
+//         // res.end("Ici le message de mon serveur")
+//     })
+// })
+// serveur.listen(8080, () => console.log('Le serveur est lancé...'))
 
 
 const http = require('http')
